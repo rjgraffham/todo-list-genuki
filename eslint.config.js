9dff1d355f09beff5093980311391af5c48895ff
Robert Graffham <robert.graffham@googlemail.com>
2025-02-24 22:07:01 +0000

    Switch back to module syntax for eslint config, enable recommended lints and browser globals.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,14 @@
-module.exports = [
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
     {
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            }
+        },
         rules: {}
     }
 ];
