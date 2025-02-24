c49c9ebb2c1cdeae07c489b6240b37aeb50e2305
Robert Graffham <robert.graffham@googlemail.com>
2025-02-24 16:36:33 +0000

    Adjust spacing of early return in Add Item handler

--- src/js/app.js
+++ src/js/app.js
@@ -106,9 +106,7 @@
         const id = crypto.randomUUID();
 
         const text = itemEntry.value.trim();
-        if(text === "") {
-            return;
-        }
+        if(text === "") { return; }
 
         const timestamp = Date.now();
 
